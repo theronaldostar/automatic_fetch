3572a3938b07d36eae81f453b169e3885ee1b042
Ronaldo S <78436286+theronaldostar@users.noreply.github.com>
2021-05-08 09:18:27 -0300

    Update request.js

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 $Ajax({
-	link: "", // URL da api-rest
+	url: "", // URL da api-rest
 	method: "", // GET, POST, PUT, DELETE...
-	body: {}, // Corpo da requisição
+	data: {}, // Corpo da requisição
 	cors: false, // Enviar este parâmetro como "true" caso queira efetuar requisições.. ou envio com bloqueio CORS
 	dataType: "json", // Retorno da requisição, "json" ou "text" padrão é json
 	success: (event) => {}, // Retorno de Sucesso.
