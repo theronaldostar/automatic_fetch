de7c2aec7325ede357bc4c607f3618adf1fa0a5a
Ronaldo S <78436286+theronaldostar@users.noreply.github.com>
2021-05-08 09:22:02 -0300

    Update request.js

--- request.js
+++ request.js
@@ -1,9 +1,10 @@
 $Ajax({
 	url: "", // URL da api-rest
-	method: "", // GET, POST, PUT, DELETE...
-	data: {}, // Corpo da requisição
-	cors: false, // Enviar este parâmetro como "true" caso queira efetuar requisições.. ou envio com bloqueio CORS
-	dataType: "json", // Retorno da requisição, "json" ou "text" padrão é json
-	success: (event) => {}, // Retorno de Sucesso.
+	method: "", // GET, POST, PUT, DELETE...;
+	headers: {}, // Cabeçalhos da requisição;
+	data: {}, // Corpo da requisição;
+	cors: false, // Enviar este parâmetro como "true" caso queira efetuar requisições.. ou envio com bloqueio CORS;
+	dataType: "json", // Retorno da requisição, "json" ou "text" padrão é json;
+	success: (event) => {}, // Retorno de Sucesso;
 	error: (event) => {} // Retorno de Erro.
 });
