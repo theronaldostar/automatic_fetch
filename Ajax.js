a20518e20f982c4dab811e1ac509bae6470ef147
Ronaldo S <78436286+theronaldostar@users.noreply.github.com>
2021-05-08 09:17:08 -0300

    Update and rename newAjax.js to Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -1,18 +1,18 @@
-/** @license newAjax v1.0
- * newAjax.js
+/** @license Ajax-fetch v1.0
+ * Ajax.js
  * 
- * Copyright (c) since 2020 Boteasy, all rights reserved.
+ *TODO: Copyright (c) since 2020 Boteasy, all rights reserved.
  * 
- *? This file is open source for you to make requests via AJAX.
- *? Developed By Ronaldo from Boteasy.
- *! More information at: https://github.com/theronaldostar/newAjax
+ *? This file is open source for you to make AJAX requests;
+ *? Developed By Ronaldo from Boteasy;
+ *! More information at: https://github.com/theronaldostar/Ajax-fetch.
 */
-const $Ajax = (event) => {
+function $Ajax(event) {
 
-	let url = event.link === undefined || event.link === null || event.link === "" || event.link === {} ? `${self.origin}/` : event.link;
+	let url = event.url === undefined || event.url === null || event.url === "" || event.url === {} ? `${self.origin}/` : event.url;
 	let method = event.method === undefined || event.method === null || event.method === "" || event.method === {} ? "GET" : event.method.toUpperCase();
 	let headers = new Headers(event.headers === undefined || event.headers === null || event.headers === {} ? [] : event.headers);
-	let bodyParams = new URLSearchParams(Object.entries(event.body === undefined || event.body === null || event.body === {} ? [] : event.body));
+	let dataParams = new URLSearchParams(Object.entries(event.data === undefined || event.data === null || event.data === {} ? [] : event.data));
 
 	const dataType = event.dataType === undefined || event.dataType === null || event.dataType === "" || event.dataType === {} ? "json" : event.dataType;
 	const success = event.success === undefined || event.success === null || event.success === "" || event.success === {} ? () => {} : event.success;
@@ -20,11 +20,11 @@ const $Ajax = (event) => {
 
 	if (method !== "GET") headers.append("Content-Type", "application/x-www-form-urlencoded");
 	headers.append("Content-Type", "charset=utf-8");
-	bodyParams = bodyParams !== undefined && bodyParams !== null && method === "GET" ? `?${bodyParams.toString()}` : bodyParams.toString();
+	dataParams = dataParams !== undefined && dataParams !== null && method === "GET" ? `?${dataParams.toString()}` : dataParams.toString();
 
 	const anywhere = "//cors-anywhere.herokuapp.com/";
-	const endLink = method === "GET" ? bodyParams : "";
-	const body = bodyParams !== undefined && bodyParams !== null && method !== "GET" ? bodyParams : null;
+	const endLink = method === "GET" ? dataParams : "";
+	const data = dataParams !== undefined && dataParams !== null && method !== "GET" ? dataParams : null;
 	const link = event.cors !== undefined && event.cors === true ? anywhere+url+endLink : url+endLink;
 
 	let data = {
@@ -35,7 +35,7 @@ const $Ajax = (event) => {
 		statusText: "Error on your internet."
 	};
 
-	fetch(link, {method: method, headers: headers, body: body}).then(async response => {
+	fetch(link, {method: method, headers: headers, body: data}).then(async response => {
 		if (!response.ok) {
 			let resolve = response.text();
 			data.type = response.type;
